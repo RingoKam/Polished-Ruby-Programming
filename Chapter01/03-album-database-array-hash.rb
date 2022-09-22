dcda9b8b9d355e7a7af3d68b774561b47705704d
Ringo <ringo_kam@hotmail.com>
2022-09-21 23:31:19 -0400

    Update 03-album-database-array-hash.rb

--- Chapter01/03-album-database-array-hash.rb
+++ Chapter01/03-album-database-array-hash.rb
@@ -4,10 +4,15 @@ album_infos = 100.times.flat_map do |i|
   end
 end
 
+# More memory efficient than last 2 approach, don't have to create hash
+# faster lookup performance when loojup by albums with tracks
 albums = {}
 album_infos.each do |album, track, artist|
   album_array = albums[album] ||= [[]]
+  # index 0 of the album array contain all artist for the album
   album_array[0] << artist
+  # since we know track is an int, 
+  # we can treat it as index and save associate artist to it 
   (album_array[track] ||= []) << artist
 end
 albums.each_value do |array|
