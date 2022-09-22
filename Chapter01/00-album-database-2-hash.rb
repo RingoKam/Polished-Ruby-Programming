3a4a8b3fd7f9f40c5ce518c8c192f047ce86235d
Ringo <ringo_kam@hotmail.com>
2022-09-21 23:00:08 -0400

    Update 00-album-database-2-hash.rb
    comment

--- Chapter01/00-album-database-2-hash.rb
+++ Chapter01/00-album-database-2-hash.rb
@@ -1,3 +1,7 @@
+#
+# Consume more memory, if there are large number of album
+# Better performance for lookup
+#
 album_infos = 100.times.flat_map do |i|
   10.times.map do |j|
     ["Album #{i}", j, "Artist #{j}"]
@@ -6,8 +10,11 @@ end
 
 album_artists = {}
 album_track_artists = {}
+
 album_infos.each do |album, track, artist|
+  # Key by album name 
   (album_artists[album] ||= []) << artist
+  # Key by array of album and track
   (album_track_artists[[album, track]] ||= []) << artist
 end
 album_artists.each_value(&:uniq!)
