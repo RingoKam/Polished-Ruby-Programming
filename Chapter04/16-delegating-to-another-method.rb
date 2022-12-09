29a1eea192f6c865fd3add03706cfb06cc77779c
Ringo <ringo_kam@hotmail.com>
2022-12-09 02:39:26 -0500

    Update 16-delegating-to-another-method.rb

--- Chapter04/16-delegating-to-another-method.rb
+++ Chapter04/16-delegating-to-another-method.rb
@@ -6,11 +6,13 @@ def bar(*args, **kwargs, &block)
   [args, kwargs, block]
 end
 
+# keep old method, show warnring and call the new method
 def foo(*args, **kwargs, &block)
   warn("foo is being renamed to bar", uplevel: 1)
   bar(*args, **kwargs, &block)
 end
 
+# shortcut with Ruby 2.7
 def foo(...)
   warn("foo is being renamed to bar", uplevel: 1)
   bar(...)
